aff67a125835aa8a5edd05f5e931697a78d906aa
Isha Ghiria <ishankg99@gmail.com>
2021-07-15 14:50:24 +0530

    game 1 update

--- src/app/game1/game1.component.ts
+++ src/app/game1/game1.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-game1',
@@ -8,12 +9,51 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class Game1Component implements OnInit {
 
-  constructor(private route: ActivatedRoute, private router: Router) { 
-    
+  constructor(private route: ActivatedRoute, private router: Router) {
+
   }
+
+  page1: boolean = true;
+  showcont:boolean = false;
+  page1cnt: number = 0;
+  locations1 = [
+    { x: '30', y: '25' },
+    { x: '35', y: '60' },
+    { x: '45', y: '40' },
+    { x: '55', y: '75' },
+    { x: '45', y: '10' },
+    { x: '60', y: '25' },
+  ]
+
+  locations2 = [
+    { x: '10', y: '45' },
+    { x: '80', y: '45' },
+    { x: '45', y: '25' },
+    { x: '35', y: '75' },
+    { x: '55', y: '60' },
+    { x: '55', y: '95' },
+  ]
 
   ngOnInit(): void {
 
+
+  }
+
+  click(x: any) {
+    var temp = '#btn' + x;
+    $(temp).css('display', 'none');
+    $("#rect" + x).css('display', 'block');
+
+    this.page1cnt++;
+    if(this.page1cnt == this.locations1.length)
+    {
+      this.showcont = true;
+    }
+  }
+
+  toggle(){
+    this.page1 = !this.page1;
+    console.log(this.page1+" Page1");
   }
 
 }
