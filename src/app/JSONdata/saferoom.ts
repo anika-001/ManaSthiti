f18f8029e40b126b3b4fa36b7966eb3cfa2a7233
unknown <jhanvisdesai@gmail.com>
2021-07-16 02:48:29 +0530

    rooms final

--- src/app/JSONdata/saferoom.ts
+++ src/app/JSONdata/saferoom.ts
@@ -12,6 +12,12 @@ export let saferoom = [
         "audio":"./../../assets/Audios/saferoombg.mp3",
     },
     {
+        "name": "Calm Room",
+        // "image":
+        "link":"/house",
+        "audio":"./../../assets/Audios/saferoombg.mp3",
+    },
+    {
         "name": "Mall",
         // "image":
         "link":"/mall",
