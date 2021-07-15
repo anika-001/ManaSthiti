fc013e0f485963e7df644fcf10502019b3c670fb
Vaishnavi <vaishnavisdesai@gmail.com>
2021-07-15 16:26:20 +0530

    ..

--- src/app/comic/comic.component.ts
+++ src/app/comic/comic.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
+
 @Component({
   selector: 'app-comic',
   templateUrl: './comic.component.html',
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,35 +12,47 @@ export class DashboardComponent implements OnInit {
 
   constructor(private as: AuthService, private router: Router, private db: AngularFirestore) { }
 
+  daysofmonths = []
+  moods = ["Angry", "Anxious", "Happy", "Peaceful", "Neutral"]
   user: any;
-  idmood: any;
+  idmood: boolean = true;
+  isstory: boolean = true;
+  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
   ngOnInit(): void {
     this.as.getUserState().subscribe(res => {
       if(!res) this.router.navigate(['/signin'])
       this.user = res;
     })
-    // console.log(new Date(), new Date("Thu Jul 15 2021 15:32:49 GMT+0530 (India Standard Time)").get(), new Date().getDate())
+    for(let i = 0; i < 30; i++) this.daysofmonths.push(i);
+    var date = new Date()
+    console.log(date.getDay().toString() + " " + date.getDate().toString() + " " + date.getMonth().toString() + " " + date.getFullYear().toString(), date.getHours(), date.getMinutes(), date.getFullYear())
   }
 
-  checkmood(){
-    this.db.collection("Users").doc(this.user.uid).collection("Moods").snapshotChanges().subscribe(res => {
-      // if(res[0]);
-    });
-  }
+  // checkmood(){
+  //   this.db.collection("Users").doc(this.user.uid).collection("Moods").snapshotChanges().subscribe(res => {
+  //     // if(res[0]);
+  //   });
+  // }
 
+  // checkstory(){
+
+  // }
 
   addstory(){
     var date = new Date()
-    this.db.collection("Users").doc(this.user.uid).collection("Stories").doc(date.toString()).set({"story": "story", "Date": "Date"});
+    this.db.collection("Users").doc(this.user.uid).collection("Stories").doc(date.getDay().toString() + " " + date.getDate().toString() + " " + date.getMonth().toString() + " " + date.getFullYear().toString()).collection("Story").add({"story": "story", "Time": "Time"});
   }
 
   addtostory(){
-
+    var date = new Date()
+    this.db.collection("Users").doc(this.user.uid).collection("Stories").doc(date.getDay().toString() + " " + date.getDate().toString() + " " + date.getMonth().toString() + " " + date.getFullYear().toString()).collection("Story").add({"story": "story", "Time": "Time"});
   }
 
   addmood(mood){
     var date = new Date()
-    this.db.collection("Users").doc(this.user.uid).collection("Moods").doc(date.toString()).set({"mood": mood});
+    this.db.collection("Users").doc(this.user.uid).collection("Moods").doc(date.getDay().toString() + " " + date.getDate().toString() + " " + date.getMonth().toString() + " " + date.getFullYear().toString()).set({"mood": mood});
   }
 
 }
