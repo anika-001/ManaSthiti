71a9ff5cb8d4b8468f3b2b66f62b447d032f0592
Vaishnavi <vaishnavisdesai@gmail.com>
2021-07-15 11:44:51 +0530

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+// import { MatSelectModule } from '@angular/material';
+import { MatSelectModule } from '@angular/material/select'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SigninComponent } from './signin/signin.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig, 'OvaryAct'),
+    FormsModule,
+    ReactiveFormsModule,
+    //FontAwesomeModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    MatSelectModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBXjsT1VQpMpc3ONR7hMB8LzUrOazbtJS4",
+    authDomain: "manasthiti-d7ca0.firebaseapp.com",
+    projectId: "manasthiti-d7ca0",
+    storageBucket: "manasthiti-d7ca0.appspot.com",
+    messagingSenderId: "939657885682",
+    appId: "1:939657885682:web:47f714a2e0967388a9dd6b"
+  }
 };
 
 /*
