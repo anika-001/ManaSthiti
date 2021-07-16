d35068b3712089bf9848fd3d7710e5ba12e07522
unknown <jhanvisdesai@gmail.com>
2021-07-16 12:17:07 +0530

    plant working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -91,6 +91,7 @@ import { TorchComponent } from './torch/torch.component';
     BrowserAnimationsModule,
   
   ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
